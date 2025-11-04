1222c9b09e659eb987acd4c4ff1a9500578a0886
Marouane bounou <maroinbounou.123@gmail.com>
2025-11-04 12:32:37 +0100

    First

---
+++ Challenges/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Entre your name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello " + name);
+    }
+}
